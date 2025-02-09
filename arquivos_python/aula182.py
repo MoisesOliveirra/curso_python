804b7d2211b6366a0fa3f8228a3769a3a200ca3b
MoisesOliveirra <105131957+MoisesOliveirra@users.noreply.github.com>
2025-02-09 13:26:40 -0300

    teste de push

---
+++ arquivos_python/aula182.py
@@ -0,0 +1,10 @@
+'''Como criar ambientes virtuais'''
+'''
+Usando o terminal:
+COMANDOS:
+python -v ###### verifica qual a versão do python
+python -m venv NOME_DO_AMBIENTE_VIRTUAL ##### vai realizar um script q criará um ambiente virtual com o nome atribuído
+    Depois desse comando, o ambiente bai ser criado com um monte de arquivos automaticamente puxados
+
+
+'''
---
+++ arquivos_python/aula183.py
@@ -0,0 +1,11 @@
+'''Como criar ambientes virtuais'''
+
+'''
+Usando o terminal:
+COMANDOS:
+ambiente\Scripts\activate ### ativa o venv
+deactivate ### desativa o ambiente virtual
+pip --version ### verificad a versão do instalador de pacotes e bibliotecas
+
+
+'''
